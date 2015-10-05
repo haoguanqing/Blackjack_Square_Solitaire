04b67faa7787d46193b95a419171c5f033aa8c23
Guanqing <haoguanqing@live.com>
2015-10-05 02:19:28 -0400

    init commit

---
+++ app/src/androidTest/java/com/example/guanqing/solblackjack/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.guanqing.solblackjack;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
